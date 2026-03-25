33fe5cc9673fdee2166b9d5cb6f4850a7477398d
Srisha S Kalale <srishaskalale24cs@rnsit.ac.in>
2026-03-25 15:41:04 +0530

    added RBAC prerequisites to the DB design

--- init_db.py
+++ init_db.py
@@ -1,16 +1,33 @@
-
+from werkzeug.security import generate_password_hash
 import sqlite3
+
+master_password="vary_sacure"
+secure_hash=generate_password_hash(master_password)
+
+stud_password="root@123"
+stud_hash=generate_password_hash(stud_password)
 
 connection = sqlite3.connect('database.db')
 
 with open('schema.sql') as f:
     connection.executescript(f.read())
 
-#cur = connection.cursor()
-#cur.execute("INSERT INTO tasks (title, category) VALUES (?, ?)",('Finish Figma Design', 'School'))
+cur = connection.cursor()
+
+cur.execute("INSERT INTO users (username, password, role) VALUES (?, ?, ?)",
+            ('admin_user', secure_hash, 'admin'))
+
+cur.execute("INSERT INTO users (username, password, role) VALUES (?, ?, ?)",
+            ('student_Summit', stud_hash, 'student'))
+
+student_id = cur.lastrowid
+
+cur.execute("INSERT INTO tasks (user_id, title, category, user_name) VALUES (?, ?, ?, ?)",
+            (student_id, 'Complete Lab 1', 'School', 'Summit'))
 
 connection.commit()
 connection.close()
 
 print("Success: database.db has been created!")
+print(f"Master password: {master_password}\nStud password: {stud_password}")
 
